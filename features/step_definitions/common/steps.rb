66a6e83f3e12830832ae1ca74728d4513fbd3fdf
yuryver <yury.ortuno@fundacion-jala.org>
2017-10-03 14:23:27 -0400

    complete builder endpoint

--- features/step_definitions/common/steps.rb
+++ features/step_definitions/common/steps.rb
@@ -1,6 +1,6 @@
 Given(/^I make a '(\w+)' request to '(.+)' endpoint$/) do |method, endpoint|
   @method = method
-  @endpoint = EnpointBuilder::builder endpoint
+  @endpoint = EnpointBuilder.builder endpoint
 end
 When(/^I execute the request to the endpoint$/) do
   smoke = ApiRequest.new(@endpoint)
@@ -13,8 +13,9 @@ Then(/^I expect a '(\d+)' status code$/) do |status_code_expected|
   expect(@response.code).to eql(status_code_expected.to_i)
 end
 
-And(/^I make a '(PUT|POST)' request to '(.+)' with paramethers:$/) do |method, endpoint, paramether|
+And(/^I make a '(PUT|POST)' request to '(.+)' with:$/) do |method, endpoint, param|
   # table is a table.hashes.keys # => [:type, :slack]
   @method = method
-  @endpoint = EnpointBuilder::builderWithParamethers endpoint, paramether.raw
+  @endpoint = EnpointBuilder.param endpoint, param.raw
 end
+
--- features/support/utils/enpoint_builder.rb
+++ features/support/utils/enpoint_builder.rb
@@ -1,12 +1,16 @@
+# This module build endpoint with id and params
 module EnpointBuilder
-  def EnpointBuilder.builder enpoint
+  def self.builder(enpoint)
     enpoint.gsub('$id', $id.to_s)
   end
-  def EnpointBuilder.builderWithParamethers endpoint, paramethers
-    endpoint = builder endpoint
-    paramethers.each_with_index do |value, index|
-      (index.to_i ==0) ? endpoint = "#{endpoint}?#{value[0]}=#{value[1]}" : endpoint = "#{endpoint}&#{value[0]}=#{value[1]}"
+
+  def self.param(endpoint, param)
+    point = builder endpoint
+    param.each_with_index do |value, index|
+      params = "#{value[0].gsub(' ', '%20')}=#{value[1].gsub(' ', '%20')}"
+      point = index.to_i.zero? ? "#{point}?#{params}" : "#{point}&#{params}"
     end
-    endpoint
+    puts point
+    point
   end
 end
